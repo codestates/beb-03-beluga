6f3fdd8f20ceb06229bc656f742e42d1a5c91d8a
leesujin <jean0518@naver.com>
2022-04-08 17:43:08 +0900

    initializing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
+import { Stack } from "@mui/material";
+
+function App() {
+  return (
+    <Stack sx={{ background: "black", border: 1, height: 600 }}>
+      <AccessibilityNewIcon></AccessibilityNewIcon>
+    </Stack>
+  );
+}
+
+export default App;
