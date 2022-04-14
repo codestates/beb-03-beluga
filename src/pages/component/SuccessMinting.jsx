c2925b1ea0234765b3c150610ac4e49523b203b4
LeeJooYong <daramzycoder@gmail.com>
2022-04-14 13:19:44 +0900

    create 페이지, nft 민팅 후 이동 버튼 추가

--- src/pages/component/SuccessMinting.jsx
+++ src/pages/component/SuccessMinting.jsx
@@ -1,11 +1,26 @@
 import { Stack, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const SuccessMinting = ({ reset }) => {
   return (
     <Stack>
-      민팅에 성공했습니다!
-      <Button variant="contained" color="primary" onClick={reset}>
+      <Stack>민팅에 성공했습니다!</Stack>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={reset}
+        sx={{ marginTop: "10%", marginBottom: "10%" }}
+      >
         민팅 페이지로
+      </Button>
+      <Button
+        component={Link}
+        to="/profile"
+        variant="contained"
+        color="primary"
+        onClick={reset}
+      >
+        프로필 페이지로
       </Button>
     </Stack>
   );
