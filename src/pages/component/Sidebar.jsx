7bd75ba567b101c7a307fe35419f09f1ddfa4c9b
Elmark <kangelmark@gmail.com>
2022-04-14 17:33:54 +0900

    Sidebar 작업ing

--- src/pages/component/Sidebar.jsx
+++ src/pages/component/Sidebar.jsx
@@ -1,12 +1,46 @@
 import { Stack } from "@mui/material";
+import FilterListIcon from '@mui/icons-material/FilterList';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { ListItemIcon } from '@mui/material';
+
 
 export default function Sidebar() {
   return (
-    <Stack sx={{ border: 1, height: "100%" }}>
-      <Stack sx={{ border: 1, height: 100 }}></Stack>
-      <Stack sx={{ border: 1, height: 100 }}></Stack>
-      <Stack sx={{ border: 1, height: 100 }}></Stack>
-      <Stack sx={{ border: 1, height: 100 }}></Stack>
+    <Stack 
+    sx={{ 
+      border: 1, 
+      height: "100%", 
+      borderColor:'grey',
+    }}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-around"
+        sx={{ border: 1, height: "14.29%" }}>
+       
+        <FilterListIcon/>
+        Filter
+        <ArrowBackIcon/>
+      </Stack>
+      <Stack sx={{ border: 1, height:  "14.29%", alignItems:"left" }}>
+        Status
+        <ListItemIcon/>
+      </Stack>
+      <Stack sx={{ border: 1, height:  "14.29%" }}>
+        Price
+      </Stack>
+      <Stack sx={{ border: 1, height:  "14.29%" }}>
+        Collections
+      </Stack>
+      <Stack sx={{ border: 1, height: "14.29%" }}>
+        Chain
+      </Stack>
+      <Stack sx={{ border: 1, height:  "14.29%" }}>
+        Categories
+      </Stack>
+      <Stack sx={{ border: 1, height:  "14.29%" }}>
+        On Sale In
+      </Stack>
     </Stack>
   );
 }
